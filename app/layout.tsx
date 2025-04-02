fc6cf668b18f841491abd1dbaba2630aa0fbefbf
Tiago Azevedo <tiagogadenz@icloud.com>
2025-04-02 17:44:09 -0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type React from "react" // Added import for React
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   )
